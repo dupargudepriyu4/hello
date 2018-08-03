3588c21ce9b012fd9592dde9cc8422b7c18f700d
studentp_10 <IamSid25>
2018-08-03 16:18:00 +0530

    Initial Commit

---
+++ hello/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello
+//
+//  Created by Student P_10 on 03/08/18.
+//  Copyright © 2018 priya. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
